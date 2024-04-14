5a08bc475ee2dd71f724e7c283f4ef0b04cd0486
bung87 <crc32@qq.com>
2024-04-14 23:55:19 +0800

    remove posix unused code

--- src/osinfo/posix.nim
+++ src/osinfo/posix.nim
@@ -4,23 +4,6 @@
 when not defined(posix):
   {.error: "This module is only supported on POSIX".}
 
-when false:
-  type
-    Tstatfs {.importc: "struct statfs64",
-              header: "<sys/statfs.h>", final, pure.} = object
-      f_type: int
-      f_bsize: int
-      f_blocks: int
-      f_bfree: int
-      f_bavail: int
-      f_files: int
-      f_ffree: int
-      f_fsid: int
-      f_namelen: int
-
-  proc statfs(path: string, buf: var Tstatfs): int {.
-    importc, header: "<sys/vfs.h>".}
-
 type
   Utsname* {.importc: "struct utsname",
               header: "<sys/utsname.h>",
