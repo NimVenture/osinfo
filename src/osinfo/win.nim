9be2d57d96530f38ea433ca2dc1ceca979316cc3
bung87 <crc32@qq.com>
2024-04-14 22:42:48 +0800

    refactor win

--- src/osinfo/win.nim
+++ src/osinfo/win.nim
@@ -8,24 +8,11 @@ when not defined(windows):
 
 import winlean
 
-const
-  INVALID_HANDLE_VALUE = int(- 1) # GetStockObject
 
 when not declared(useWinUnicode):
   const useWinUnicode* = not defined(useWinAnsi)
 
 type
-  TMEMORYSTATUSEX {.final, pure.} = object
-    dwLength: int32
-    dwMemoryLoad: int32
-    ullTotalPhys: int64
-    ullAvailPhys: int64
-    ullTotalPageFile: int64
-    ullAvailPageFile: int64
-    ullTotalVirtual: int64
-    ullAvailVirtual: int64
-    ullAvailExtendedVirtual: int64
-
   SYSTEM_INFO* {.final, pure.} = object
     wProcessorArchitecture*: int16
     wReserved*: int16
@@ -171,33 +158,16 @@ const
   # GetSystemMetrics
   SM_SERVERR2 = 89
 
-proc globalMemoryStatusEx*(lpBuffer: var TMEMORYSTATUSEX){.stdcall, dynlib: "kernel32",
-    importc: "GlobalMemoryStatusEx".}
-
-proc getMemoryInfo*(): TMemoryInfo =
-  ## Retrieves memory info
-  var statex: TMEMORYSTATUSEX
-  statex.dwLength = sizeof(statex).int32
-
-  globalMemoryStatusEx(statex)
-  result.MemoryLoad = statex.dwMemoryLoad
-  result.TotalPhysMem = statex.ullTotalPhys
-  result.AvailablePhysMem = statex.ullAvailPhys
-  result.TotalPageFile = statex.ullTotalPageFile
-  result.AvailablePageFile = statex.ullAvailPageFile
-  result.TotalVirtualMem = statex.ullTotalVirtual
-  result.AvailableVirtualMem = statex.ullAvailExtendedVirtual
-
-proc getVersionEx*(lpVersionInformation: var TOSVERSIONINFOEX): WINBOOL{.stdcall,
+proc getVersionEx(lpVersionInformation: var TOSVERSIONINFOEX): WINBOOL{.stdcall,
     dynlib: "kernel32", importc: "GetVersionExA".}
 
-proc getProcAddress*(hModule: int, lpProcName: cstring): pointer{.stdcall,
+proc getProcAddress(hModule: int, lpProcName: cstring): pointer{.stdcall,
     dynlib: "kernel32", importc: "GetProcAddress".}
 
-proc getModuleHandleA*(lpModuleName: cstring): int{.stdcall,
+proc getModuleHandleA(lpModuleName: cstring): int{.stdcall,
      dynlib: "kernel32", importc: "GetModuleHandleA".}
 
-proc rtlGetVersion*(lpVersionInformation: var TOSVERSIONINFOEX): cint{.stdcall, dynlib: "ntdll",
+proc rtlGetVersion(lpVersionInformation: var TOSVERSIONINFOEX): cint{.stdcall, dynlib: "ntdll",
   importc: "RtlGetVersion".}
 
 proc getVersionInfo*(): TVersionInfo =
@@ -224,11 +194,11 @@ proc getVersionInfo*(): TVersionInfo =
   result.SuiteMask = osvi.wSuiteMask
   result.ProductType = osvi.wProductType
 
-proc getProductInfo*(majorVersion, minorVersion, SPMajorVersion,
+proc getProductInfo(majorVersion, minorVersion, SPMajorVersion,
                      SPMinorVersion: int): int =
   ## Retrieves Windows' ProductInfo, this function only works in Vista and 7
   let pGPI = cast[proc (dwOSMajorVersion, dwOSMinorVersion,
-              dwSpMajorVersion, dwSpMinorVersion: int32, outValue: Pint32){.stdcall.}](getProcAddress(
+              dwSpMajorVersion, dwSpMinorVersion: int32, outValue: ptr int32){.stdcall.}](getProcAddress(
                 getModuleHandleA("kernel32.dll"), "GetProductInfo"))
 
   if pGPI != nil:
@@ -238,10 +208,10 @@ proc getProductInfo*(majorVersion, minorVersion, SPMajorVersion,
   else:
     return PRODUCT_UNDEFINED
 
-proc getSystemInfo*(lpSystemInfo: LPSYSTEM_INFO){.stdcall, dynlib: "kernel32",
+proc getSystemInfo(lpSystemInfo: LPSYSTEM_INFO){.stdcall, dynlib: "kernel32",
     importc: "GetSystemInfo".}
 
-proc getSystemInfo*(): TSYSTEM_INFO =
+proc getSystemInfo(): TSYSTEM_INFO =
   ## Returns the SystemInfo
 
   # Use GetNativeSystemInfo if it's available
@@ -256,196 +226,197 @@ proc getSystemInfo*(): TSYSTEM_INFO =
 
   return systemi
 
-proc getSystemMetrics*(nIndex: int32): int32{.stdcall, dynlib: "user32",
+proc getSystemMetrics(nIndex: int32): int32{.stdcall, dynlib: "user32",
     importc: "GetSystemMetrics".}
 
-proc `$`*(osvi: TVersionInfo): string =
+proc getOsInfo*(osvi: TVersionInfo, includeEdition = false, includeSp = false, includeBuild = false, includeArch = false): string =
   ## Turns a VersionInfo object into a string
   if osvi.platformID == VER_PLATFORM_WIN32_NT and osvi.majorVersion > 4:
-    result = "Microsoft "
+    result = "Microsoft"
 
     let si = getSystemInfo()
     # Test for the specific product
     if osvi.majorVersion == 10:
       if osvi.buildNumber >= 22000:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows 11 ")
-        else: result.add("Windows Server 2022 ")
+          result.add(" Windows 11")
+        else: result.add(" Windows Server 2022")
       else:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows 10 ")
-        else: result.add("Windows Server 2016 ")
+          result.add(" Windows 10")
+        else: result.add(" Windows Server 2016")
     if osvi.majorVersion == 6:
       if osvi.minorVersion == 0:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows Vista ")
-        else: result.add("Windows Server 2008 ")
+          result.add(" Windows Vista")
+        else: result.add(" Windows Server 2008")
       elif osvi.minorVersion == 1:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows 7 ")
-        else: result.add("Windows Server 2008 R2 ")
+          result.add(" Windows 7")
+        else: result.add(" Windows Server 2008 R2")
       elif osvi.minorVersion == 2:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows 8 ")
-        else: result.add("Windows Server 2012 ")
+          result.add(" Windows 8")
+        else: result.add(" Windows Server 2012")
       elif osvi.minorVersion == 3:
         if osvi.ProductType == VER_NT_WORKSTATION:
-          result.add("Windows 8.1 ")
-        else: result.add("Windows Server 2012 R2 ")
+          result.add(" Windows 8.1")
+        else: result.add(" Windows Server 2012 R2")
 
     # The GetProductInfo return result works when dwOSMajorVersion minimum value is 6.
-    if osvi.majorVersion >= 6:
+    if includeEdition and osvi.majorVersion >= 6:
       let dwType = getProductInfo(osvi.majorVersion, osvi.minorVersion, 0, 0)
       case dwType
       of PRODUCT_PRO_WORKSTATION:
-        result.add("Pro for Workstations")
+        result.add(" Pro for Workstations")
       of PRODUCT_PRO_WORKSTATION_N:
-        result.add("Pro for Workstations N")
+        result.add(" Pro for Workstations N")
       of PRODUCT_EDUCATION:
-        result.add("Education")
+        result.add(" Education")
       of PRODUCT_EDUCATION_N:
-        result.add("Education N")
+        result.add(" Education N")
       of PRODUCT_ULTIMATE:
-        result.add("Ultimate Edition")
+        result.add(" Ultimate")
       of PRODUCT_PROFESSIONAL:
-        result.add("Professional")
+        result.add(" Professional")
       of PRODUCT_HOME_PREMIUM:
-        result.add("Home Premium Edition")
+        result.add(" Home Premium")
       of PRODUCT_HOME_BASIC:
-        result.add("Home Basic Edition")
+        result.add(" Home Basic")
       of PRODUCT_ENTERPRISE:
-        result.add("Enterprise Edition")
+        result.add(" Enterprise")
       of PRODUCT_ENTERPRISE_E:
-        result.add("Enterprise E")
+        result.add(" Enterprise E")
       of PRODUCT_ENTERPRISE_EVALUATION:
-        result.add("Enterprise Evaluation")
+        result.add(" Enterprise Evaluation")
       of PRODUCT_ENTERPRISE_N:
-        result.add("Enterprise N")
+        result.add(" Enterprise N")
       of PRODUCT_ENTERPRISE_N_EVALUATION:
-        result.add("Enterprise N Evaluation")
+        result.add(" Enterprise N Evaluation")
       of PRODUCT_ENTERPRISE_S:
-        result.add("Enterprise 2015 LTSB")
+        result.add(" Enterprise 2015 LTSB")
       of PRODUCT_ENTERPRISE_S_EVALUATION:
-        result.add("Enterprise 2015 LTSB Evaluation")
+        result.add(" Enterprise 2015 LTSB Evaluation")
       of PRODUCT_ENTERPRISE_S_N:
-        result.add("Enterprise 2015 LTSB N")
+        result.add(" Enterprise 2015 LTSB N")
       of PRODUCT_ENTERPRISE_S_N_EVALUATION:
-        result.add("Enterprise 2015 LTSB N Evaluation")
+        result.add(" Enterprise 2015 LTSB N Evaluation")
       of PRODUCT_BUSINESS:
-        result.add("Business Edition")
+        result.add(" Business")
       of PRODUCT_STARTER:
-        result.add("Starter Edition")
+        result.add(" Starter")
       of PRODUCT_CLUSTER_SERVER:
-        result.add("Cluster Server Edition")
+        result.add(" Cluster Server")
       of PRODUCT_DATACENTER_SERVER:
-        result.add("Datacenter Edition")
+        result.add(" Datacenter")
       of PRODUCT_DATACENTER_SERVER_CORE:
-        result.add("Datacenter Edition (core installation)")
+        result.add(" Datacenter (core installation)")
       of PRODUCT_DATACENTER_SERVER_CORE_V:
-        result.add("Server Datacenter without Hyper-V (core installation)")
+        result.add(" Server Datacenter without Hyper-V (core installation)")
       of PRODUCT_DATACENTER_SERVER_V:
-        result.add("Server Datacenter without Hyper-V (full installation)")
+        result.add(" Server Datacenter without Hyper-V (full installation)")
       of PRODUCT_ENTERPRISE_SERVER:
-        result.add("Server Enterprise (full installation)")
+        result.add(" Server Enterprise (full installation)")
       of PRODUCT_ENTERPRISE_SERVER_CORE:
-        result.add("Server Enterprise (core installation)")
+        result.add(" Server Enterprise (core installation)")
       of PRODUCT_ENTERPRISE_SERVER_CORE_V:
-        result.add("Server Enterprise without Hyper-V (core installation)")
+        result.add(" Server Enterprise without Hyper-V (core installation)")
       of PRODUCT_ENTERPRISE_SERVER_V:
-        result.add("Server Enterprise without Hyper-V (full installation)")
+        result.add(" Server Enterprise without Hyper-V (full installation)")
       of PRODUCT_ENTERPRISE_SERVER_IA64:
-        result.add("Server Enterprise for Itanium-based Systems")
+        result.add(" Server Enterprise for Itanium-based Systems")
       of PRODUCT_SMALLBUSINESS_SERVER:
-        result.add("Small Business Server")
+        result.add(" Small Business Server")
       of PRODUCT_STANDARD_SERVER:
-        result.add("Standard Edition")
+        result.add(" Standard")
       of PRODUCT_STANDARD_SERVER_CORE:
-        result.add("Standard Edition (core installation)")
+        result.add(" Standard (core installation)")
       of PRODUCT_WEB_SERVER:
-        result.add("Web Server Edition")
+        result.add(" Web Server")
       of PRODUCT_STORAGE_ENTERPRISE_SERVER:
-        result.add("Storage Server Enterprise")
+        result.add(" Storage Server Enterprise")
       of PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE:
-        result.add("Storage Server Enterprise (core installation) ")
+        result.add(" Storage Server Enterprise (core installation) ")
       of PRODUCT_STORAGE_EXPRESS_SERVER:
-        result.add("Storage Server Express")
+        result.add(" Storage Server Express")
       of PRODUCT_STORAGE_STANDARD_SERVER:
-        result.add("Storage Server Standard")
+        result.add(" Storage Server Standard")
       of PRODUCT_STORAGE_WORKGROUP_SERVER:
-        result.add("Storage Server Workgroup")
+        result.add(" Storage Server Workgroup")
       else:
         discard
     # End of Windows 6.*
 
-    if osvi.majorVersion == 5 and osvi.minorVersion == 2:
+    elif osvi.majorVersion == 5 and osvi.minorVersion == 2:
       if getSystemMetrics(SM_SERVERR2) != 0:
-        result.add("Windows Server 2003 R2, ")
+        result.add(" Windows Server 2003 R2")
       elif (osvi.SuiteMask and VER_SUITE_PERSONAL) != 0: # Not sure if this will work
-        result.add("Windows Storage Server 2003 ")
+        result.add(" Windows Storage Server 2003")
       elif (osvi.SuiteMask and VER_SUITE_WH_SERVER) != 0:
-        result.add("Windows Home Server ")
-      elif osvi.ProductType == VER_NT_WORKSTATION and
-          si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64:
-        result.add("Windows XP Professional x64 Edition ")
+        result.add(" Windows Home Server")
+      elif osvi.ProductType == VER_NT_WORKSTATION: 
+      # and
+      #     si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64:
+        result.add(" Windows XP Professional")
       else:
-        result.add("Windows Server 2003, ")
+        result.add(" Windows Server 2003")
 
       # Test for the specific product
-      if osvi.ProductType != VER_NT_WORKSTATION:
+      if includeEdition and osvi.ProductType != VER_NT_WORKSTATION:
         if si.wProcessorArchitecture.uint == PROCESSOR_ARCHITECTURE_IA64:
           if (osvi.SuiteMask and VER_SUITE_DATACENTER) != 0:
-            result.add("Datacenter Edition for Itanium-based Systems")
+            result.add(" Datacenter for Itanium-based Systems")
           elif (osvi.SuiteMask and VER_SUITE_ENTERPRISE) != 0:
-            result.add("Enterprise Edition for Itanium-based Systems")
-        elif si.wProcessorArchitecture.uint == PROCESSOR_ARCHITECTURE_AMD64:
-          if (osvi.SuiteMask and VER_SUITE_DATACENTER) != 0:
-            result.add("Datacenter x64 Edition")
-          elif (osvi.SuiteMask and VER_SUITE_ENTERPRISE) != 0:
-            result.add("Enterprise x64 Edition")
-          else:
-            result.add("Standard x64 Edition")
+            result.add(" Enterprise for Itanium-based Systems")
+        # elif si.wProcessorArchitecture.uint == PROCESSOR_ARCHITECTURE_AMD64:
+        #   if (osvi.SuiteMask and VER_SUITE_DATACENTER) != 0:
+        #     result.add("Datacenter x64 Edition")
+        #   elif (osvi.SuiteMask and VER_SUITE_ENTERPRISE) != 0:
+        #     result.add("Enterprise x64 Edition")
+        #   else:
+        #     result.add("Standard x64 Edition")
         else:
           if (osvi.SuiteMask and VER_SUITE_COMPUTE_SERVER) != 0:
-            result.add("Compute Cluster Edition")
+            result.add(" Compute Cluster")
           elif (osvi.SuiteMask and VER_SUITE_DATACENTER) != 0:
-            result.add("Datacenter Edition")
+            result.add( "Datacenter")
           elif (osvi.SuiteMask and VER_SUITE_ENTERPRISE) != 0:
-            result.add("Enterprise Edition")
+            result.add(" Enterprise")
           elif (osvi.SuiteMask and VER_SUITE_BLADE) != 0:
-            result.add("Web Edition")
+            result.add(" Web")
           else:
-            result.add("Standard Edition")
+            result.add(" Standard")
     # End of 5.2
 
-    if osvi.majorVersion == 5 and osvi.minorVersion == 1:
-      result.add("Windows XP ")
+    elif osvi.majorVersion == 5 and osvi.minorVersion == 1:
+      result.add(" Windows XP")
       if (osvi.SuiteMask and VER_SUITE_PERSONAL) != 0:
-        result.add("Home Edition")
+        result.add(" Home Edition")
       else:
-        result.add("Professional")
+        result.add(" Professional")
     # End of 5.1
 
-    if osvi.majorVersion == 5 and osvi.minorVersion == 0:
-      result.add("Windows 2000 ")
+    elif osvi.majorVersion == 5 and osvi.minorVersion == 0:
+      result.add(" Windows 2000")
       if osvi.ProductType == VER_NT_WORKSTATION:
-        result.add("Professional")
+        result.add(" Professional")
       else:
         if (osvi.SuiteMask and VER_SUITE_DATACENTER) != 0:
-          result.add("Datacenter Server")
+          result.add(" Datacenter Server")
         elif (osvi.SuiteMask and VER_SUITE_ENTERPRISE) != 0:
-          result.add("Advanced Server")
+          result.add(" Advanced Server")
         else:
-          result.add("Server")
+          result.add(" Server")
     # End of 5.0
 
     # Include service pack (if any) and build number.
-    if len(osvi.SPVersion) > 0:
+    if includeSp and len(osvi.SPVersion) > 0:
       result.add(" ")
       result.add(osvi.SPVersion)
+    if includeBuild:
+      result.add(" (build " & $osvi.buildNumber & ")")
 
-    result.add(" (build " & $osvi.buildNumber & ")")
-
-    if osvi.majorVersion >= 6:
+    if includeArch and osvi.majorVersion >= 6:
       if si.wProcessorArchitecture.uint == PROCESSOR_ARCHITECTURE_AMD64:
         result.add(", 64-bit")
       elif si.wProcessorArchitecture.uint == PROCESSOR_ARCHITECTURE_INTEL:
@@ -454,41 +425,10 @@ proc `$`*(osvi: TVersionInfo): string =
     # Windows 98 etc...
     result = "Unknown version of windows[Kernel version <= 4]"
 
-proc getFileSize*(file: string): BiggestInt =
-  ## Retrieves the size of the specified file.
-  var fileData: WIN32_FIND_DATA
-
-  when useWinUnicode:
-    var aa = newWideCString(file)
-    let hFile = findFirstFileW(aa, fileData)
-  else:
-    let hFile = findFirstFileA(file, fileData)
-
-  if hFile == INVALID_HANDLE_VALUE:
-    raise newException(IOError, $getLastError())
-
-  return fileData.nFileSizeLow
-
-proc getDiskFreeSpaceEx*(lpDirectoryName: cstring, lpFreeBytesAvailableToCaller,
-                         lpTotalNumberOfBytes,
-                         lpTotalNumberOfFreeBytes: var FileTime): WINBOOL{.
-    stdcall, dynlib: "kernel32", importc: "GetDiskFreeSpaceExA".}
-
-proc getPartitionInfo*(partition: string): TPartitionInfo =
-  ## Retrieves partition info, for example ``partition`` may be ``"C:\"``
-  var freeBytes, totalBytes, totalFreeBytes: FileTime
-  discard getDiskFreeSpaceEx(partition, freeBytes, totalBytes,
-                               totalFreeBytes)
-  return (freeBytes, totalBytes)
+proc `$`*(osvi: TVersionInfo): string = osvi.getOsInfo(true, true, true, true)
 
 when isMainModule:
-  let r = getMemoryInfo()
-  echo("Memory load: ", r.MemoryLoad, "%")
 
   let osvi = getVersionInfo()
 
   echo($osvi)
-
-  #echo(getFileSize(r"lib\impure\osinfo_win.nim") div 1024, " KB")
-
-  echo(rdFileTime(getPartitionInfo(r"C:\")[0]))
